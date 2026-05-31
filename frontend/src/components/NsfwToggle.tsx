b9c7ef5e3e91e8e8907cdfef2073cc55f7cf21f5
LordDeatHunter <desuhansa@gmail.com>
2026-06-01 01:04:11 +0200

    Change nsfw toggle to switch

--- frontend/src/components/NsfwToggle.tsx
+++ frontend/src/components/NsfwToggle.tsx
@@ -6,14 +6,21 @@ interface NsfwToggleProps {
 }
 
 const NsfwToggle: Component<NsfwToggleProps> = (props) => (
-  <label class="nsfw-toggle">
+  <label class="nsfw-toggle" for="nsfwToggle">
     <input
       id="nsfwToggle"
+      class="nsfw-toggle-input"
       type="checkbox"
+      role="switch"
       checked={props.store.showNsfw()}
       onChange={(e) => props.store.setShowNsfw(e.currentTarget.checked)}
     />
-    <span>Show NSFW</span>
+    <span class="nsfw-toggle-track" aria-hidden="true">
+      <span class="nsfw-toggle-thumb" />
+    </span>
+    <span class="nsfw-toggle-label">
+      {props.store.showNsfw() ? "NSFW Visible" : "NSFW Hidden"}
+    </span>
   </label>
 );
 
